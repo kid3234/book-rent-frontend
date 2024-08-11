9ecab0fb36464da86223e48e374f0624e2a971ee
kid3234 <birhaneaddis2@gmail.co>
2024-08-11 12:02:32 +0300

    update

--- src/pages/Admin/Owners/index.jsx
+++ src/pages/Admin/Owners/index.jsx
@@ -5,7 +5,27 @@ import ReactVirtualizedTable from "../../../components/Table";
 
 import DeletePopup from "../../../components/DeletePopup";
 import axios from "axios";
-import SeeMore from "../../../components/SeeMore";
+// import SeeMore from "../../../components/SeeMore";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: 4,
+  borderRadius: 4,
+};
 
 
 function Owners() {
@@ -97,12 +117,75 @@ function Owners() {
         </div>
       </div>
 
-      <SeeMore data={data} handleClose={handleClosesee} open={opensee} />
+      {/* <SeeMore data={data} handleClose={handleClosesee} open={opensee} /> */}
       <DeletePopup
         handleClose={handleCloseDelete}
         open={opendelete}
         data={owner}
       />
+       <Modal
+      keepMounted
+      open={opensee}
+      onClose={handleClosesee}
+      aria-labelledby="keep-mounted-modal-title"
+      aria-describedby="keep-mounted-modal-description"
+    >
+      <Box sx={style}>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "100%" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="name"
+            label="Name"
+            value={data?.name}
+            InputProps={{
+              readOnly: true,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="email"
+            label="Email address"
+            value={data?.email}
+            InputProps={{
+              readOnly: true,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="location"
+            label="Location"
+            value={data?.location}
+            InputProps={{
+              readOnly: true,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="phone"
+            label="Phone Number"
+            value={data?.phone}
+            InputProps={{
+              readOnly: true,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Box>
+      </Box>
+    </Modal>
     </div>
   );
 }
